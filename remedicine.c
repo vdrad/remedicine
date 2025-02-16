37e88068a83995a026af94276f37520761459e87
Victor Gomes <victor.drad.g@gmail.com>
2025-02-16 11:17:13 -0300

    Corrigida a lógica de início do programa

--- remedicine.c
+++ remedicine.c
@@ -14,6 +14,8 @@
 #include "inc/alarm/alarm.h"                // Biblioteca do Alarme.
 #include "inc/remedios/remedios.h"          // Biblioteca de cadastro dos remédios.
 
+// extern struct repeating_timer display_timer;
+
 /* ==================================== CÓDIGO PRINCIPAL ==================================== */
 int main() {
     stdio_init_all();       // Inicializa entradas e saídas
@@ -26,17 +28,26 @@ int main() {
     alarm_init();           // Inicializa o Alarme
 
     // Espera até que o usuário pressione o Botão A ou B para avançar no código
-    while(!get_a_button_state() || get_b_button_state());
+    uint8_t a_button_pressed = get_a_button_state();
+    uint8_t b_button_pressed = get_b_button_state();
+    while((!a_button_pressed) && (!b_button_pressed)) {
+        a_button_pressed = get_a_button_state();
+        b_button_pressed = get_b_button_state();
+    };
 
     // Utiliza o recurso timer para atualizar o horário no display OLED
     static struct repeating_timer display_timer;                                   // Cria a estrutura de timer ciclico
     add_repeating_timer_us(100 *1000, display_current_time, NULL, &display_timer); // Configura a função button_callback() para ser executada a cada 100 ms
 
-    // Não faz mais nada. Loop infinito.
+    // Loop infinito
     while (true) {
-        sleep_ms(100);
+        // Constantemente checa se há algum alarme para ser acionado.
         int8_t medicine_index = check_for_medicine_alarm();
-        if (medicine_index > -1) notify_medicine(&remedios[medicine_index]);
+        if (medicine_index > -1) {
+            cancel_repeating_timer(&display_timer);     // Desabilita atualização padrão do display para exibir informações do remédio
+            notify_medicine(&remedios[medicine_index]); // Notifica ao usuário do remédio a ser tomado
+            add_repeating_timer_us(100 *1000, display_current_time, NULL, &display_timer);  // Volta a exibir o horário atual
+        }
     }
 
     return 0;
